51b2b5a960b495b20629635807265d1610c256be
Guirissi <106929790+Guirissi@users.noreply.github.com>
2022-06-25 00:25:27 -0300

    Update LDR_TCC_PROJETO.ino

--- LDR_TCC_PROJETO.ino
+++ LDR_TCC_PROJETO.ino
@@ -1,48 +1,46 @@
 
 // AJUSTES DE VALORES
-#define OBSTRUCAO 100           //VALOR PARA CHECAGEM DE OBSTRUÇAO
-#define VALOR_MINIMO 800        //VALOR MINIMO DE CLARIDADE
+#define OBSTRUCAO         //VALOR PARA CHECAGEM DE OBSTRUÇAO
+#define VALOR_MINIMO      //VALOR MINIMO DE CLARIDADE
 
 // PINOS DE LED
-#define LED_LDR  6              
-#define LEDSTRIP 7
+#define LED_LDR  6            
+#define LEDSTRIP 
 
 // PINOS LDR
-#define LDR1 A4 
-#define LDR2 A5
+#define LDR1 
+#define LDR2 
 
 //VARIAVEIS DE LEITURA DOS LDR'S
 int  leitura_ldr1,leitura_ldr2;
 
 void setup() {
-pinMode(INPUT, LDR1);
-pinMode(INPUT, LDR2);
+ 
+pinMode(LDR1, INPUT );
+pinMode(LDR1, INPUT);
 
-pinMode(OUTPUT, LEDSTRIP);
-pinMode(OUTPUT, LED_LDR);
+pinMode(LEDSTRIP, OUTPUT);
+
 }
 
 void loop() {
  
- if(tempo=TEMPO_LDR){
  leitura_ldr1=analogRead(LDR1);
  leitura_ldr2=analogRead(LDR2);
  //------------------LEITURA DE OBSTRÇÃO------------------------------
  if((leitura_ldr1>leitura_ldr2+OBSTRUCAO)||(leitura_ldr2>leitura_ldr1+OBSTRUCAO))
  {
-  digitalWrite(HIGH,LED_LDR)
-  NotificaUsuario(); 
-  NotificaLCD();
+  return 'O'; //Retorna Valor de obstrução para as funçeos de notificações 
  }
  
  if((ldr1+ldr2/2)<VALOR_MINIMO)
   {
-     digitalWrite(HIGH,LED_LDR)
+     digitalWrite(LEDSTRIP,HIGH);
     
   }
   else 
-   digitalWrite(LOW,LED_LDR)
+   digitalWrite(LEDSTRIP,HIGH);
 
- }
+  }
  
 }
