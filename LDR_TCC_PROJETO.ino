78b608e7cd3df414d5c7cacdfea44bef89343377
Guirissi <106929790+Guirissi@users.noreply.github.com>
2022-06-25 00:18:04 -0300

    Update LDR_TCC_PROJETO.ino

--- LDR_TCC_PROJETO.ino
+++ LDR_TCC_PROJETO.ino
@@ -1,26 +1,25 @@
 
 // AJUSTES DE VALORES
-#define OBSTRUCAO 100 //VALOR PARA CHECAGEM DE OBSTRUÇAO
-#define TEMPO_LDR 1 //TEMPO PARA CHAMAR A FUNÇAO(IRA PRA O MAIN FUTURAMENTE)
-#define VALOR_MINIMO 800 //VALOR MINIMO DE CLARIDADE
+#define OBSTRUCAO 100           //VALOR PARA CHECAGEM DE OBSTRUÇAO
+#define VALOR_MINIMO 800        //VALOR MINIMO DE CLARIDADE
+
 // PINOS DE LED
-#define LED_LDR  6 //PINO
-#define LEDSTRIP 7 //PINO
+#define LED_LDR  6              
+#define LEDSTRIP 7
 
 // PINOS LDR
 #define LDR1 A4 
 #define LDR2 A5
+
 //VARIAVEIS DE LEITURA DOS LDR'S
 int  leitura_ldr1,leitura_ldr2;
-int  tempo=1;
-
 
 void setup() {
-pinMode(INPUT,LDR1);
-pinMode(INPUT,LDR2);
+pinMode(INPUT, LDR1);
+pinMode(INPUT, LDR2);
 
-pinMode(OUTPUT,LEDSTRIP);
-pinMode(OUTPUT,LED_LDR);
+pinMode(OUTPUT, LEDSTRIP);
+pinMode(OUTPUT, LED_LDR);
 }
 
 void loop() {
