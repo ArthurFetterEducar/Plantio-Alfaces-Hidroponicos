899a432c56a132a97568f9c094ff7e5c086bf1a4
ArthurFetterEducar <106825662+ArthurFetterEducar@users.noreply.github.com>
2022-06-28 15:17:48 -0300

    Sensor Boia Cima revisão tipografia

--- BoiaCima.ino
+++ BoiaCima.ino
@@ -1,18 +1,12 @@
 #define SENS_BOIA_C                           //Pino Analogico para a boia 
 
-void setup() {
+int leitura_boiaC;                            //Variaveis de Leitura da Boia
+
+
+int boiaCima() {
+  pinMode(SENS_BOIA_C, INPUT);
   
-pinMode(SENS_BOIA_B, INPUT);
-
-}
-  void loop() {
-
-  int leitura_boiaC;                          //Variaveis de Leitura da Boia
-
-  leitura_boiaC = analogRead(SENS_BOIA_B);    //Leitura do pino
-                                              //Checa Esvaziamento 
-  if(leitura_boiaC==0)                        //Nivel: Elevado               
-     return 'V';
-  if(leitura_boiaC==1)                        //Nivel: Normal          
-     return 'F';   
+  leitura_boiaC = digitalRead(SENS_BOIA_B);    //Leitura do pino
+                                              
+  return leitura_boisaC;
 }
