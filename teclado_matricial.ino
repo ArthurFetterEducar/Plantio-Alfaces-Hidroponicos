1c9891b59a639daa58a0c04450b07c52a48ffe70
ArthurFetterEducar <106825662+ArthurFetterEducar@users.noreply.github.com>
2022-06-28 14:48:11 -0300

    Revisão da tipografia

--- teclado_matricial.ino
+++ teclado_matricial.ino
@@ -1,63 +1,69 @@
-int b1,b2,b3,b4,b5;
-int botao1,botao2,botao3,botao4,botao5;
-int comando = 0;
+#define PIN_TECLA_1
+#define PIN_TECLA_2
+#define PIN_TECLA_3
+#define PIN_TECLA_4
+#define PIN_TECLA_5
 
-void setup() 
-{
- pinMode(botao1,INPUT);
- pinMode(botao2,INPUT);
- pinMode(botao3,INPUT);
- pinMode(botao4,INPUT);
- pinMode(botao5,INPUT); 
-}
+int debounce1, debounce2, debounce3, debounce4, debounce5;
+int tecla1, tecla2, tecla3, tecla4, tecla5;
+char comando;
 
-void loop() 
+
+char teclado_matricial()
 {
+ pinMode(PIN_TECLA_1, INPUT);
+ pinMode(PIN_TECLA_2, INPUT);
+ pinMode(PIN_TECLA_3, INPUT);
+ pinMode(PIN_TECLA_4, INPUT);
+ pinMode(PIN_TECLA_5, INPUT);
+ 
+ tecla1 = digitalRead(PIN_TECLA_1);
+ tecla2 = digitalRead(PIN_TECLA_2);
+ tecla3 = digitalRead(PIN_TECLA_3);
+ tecla4 = digitalRead(PIN_TECLA_4);
+ tecla5 = digitalRead(PIN_TECLA_5);
+
+ comando = ' ';
+ 
  // Botão da esquerda
- if(botao1 == 0 && b1 == 0)
- {
-  comando = 1;             // Comando que sera retornado
-  b1 = 1;                  // Impede a repetição 
+ if(tecla1 == 0 && debounce1 == 0){
+  comando = 'C';                         // Comando que sera retornado
+  debounce1 = 1;                         // Impede a repetição 
  }
- if(botao1 == 1)
-  b1 = 0;                  // Sinaliza que o boão foi solto
+ if(tecla1 == 1)
+  debounce1 = 0;                         // Sinaliza que o boão foi solto
 
  // Botão da direita
- if(botao2 == 0 && b2 == 0)
- {
-  comando = 2;             // Comando que sera retornado
-  b2 = 2;                  // Impede a repetição 
+ if(tecla2 == 0 && debounce2 == 0){
+  comando = 'B';                         // Comando que sera retornado
+  debounce2 = 2;                         // Impede a repetição 
  }
- if(botao2 == 1)
-  b2 = 0;                  // Sinaliza que o boão foi solto
+ if(tecla2 == 1)
+  debounce2 = 0;                         // Sinaliza que o boão foi solto
 
  // Botão de Cima 
-  if(botao3 == 0 && b3 == 0)
- {
-  comando = 3;             // Comando que sera retornado
-  b3 = 1;                  // Impede a repetição 
+  if(tecla3 == 0 && debounce3 == 0){
+  comando = 'E';                         // Comando que sera retornado
+  debounce3 = 1;                         // Impede a repetição 
  }
- if(botao3 == 1)
-  b3 = 0;                  // Sinaliza que o boão foi solto
-  
- //Botão Baixo
-  if(botao4 == 0 && b4 == 0)
- {
-  comando = 4;             // Comando que sera retornado
-  b4 = 1;                  // Impede a repetição 
- }
- if(botao4 == 1)
-  b4 = 0;                  // Sinaliza que o boão foi solto 
-  
- //Botão de Confirmar
-  if(botao5 == 0 && b5 == 0)
- {
-  comando = 5;             // Comando que sera retornado
-  b5 = 1;                  // Impede a repetição 
- }
- if(botao5 == 1)
-  b5 = 0;                  // Sinaliza que o boão foi solto   
- 
- return comando;
+ if(tecla3 == 1)
+  debounce3 = 0;                         // Sinaliza que o boão foi solto
 
+ //Botão Baixo
+  if(tecla4 == 0 && debounce4 == 0){
+  comando = 'D';                         // Comando que sera retornado
+  debounce4 = 1;                         // Impede a repetição 
+ }
+ if(tecla4 == 1)
+  debounce4 = 0;                         // Sinaliza que o boão foi solto 
+
+ //Botão de Confirmar
+  if(tecla5 == 0 && debounce5 == 0){
+  comando = 'e';                        // Comando que sera retornado
+  debounce5 = 1;                        // Impede a repetição 
+ }
+ if(tecla5 == 1)
+  debounce5 = 0;                        // Sinaliza que o boão foi solto   
+
+ return comando;
 }
