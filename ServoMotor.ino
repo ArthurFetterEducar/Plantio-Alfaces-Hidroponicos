11029a2ba9f2376a885fee060d4beb27f4cde65e
Martininho <108107027+Martininho@users.noreply.github.com>
2022-06-28 14:06:28 -0300

    Update ServoMotor.ino

--- ServoMotor.ino
+++ ServoMotor.ino
@@ -1,5 +1,4 @@
 #include <Servo.h>
-
 #define GrausInicial 
 #define GrausFinal 
 
@@ -7,18 +6,20 @@ Servo servo;
 
 int pos = 0;        // Posição do servo motor
 
-void setup() {
+void setup() 
+{
   servo.attach(9);  // Pino do Servo Motor
 }
-
-void loop() {
-  for (pos = GrausInicial; pos <= GrausFinal; pos += 1) {       // Servo vai ate o valor limitado
-    
-    servo.write(pos);                                           // Aplicação para o servo ir ate o local desejado 
-    delay(15);                                                  // delay para ir ate a posição
+void loop() 
+{
+  for (pos = GrausInicial; pos <= GrausFinal; pos += 1)          // Servo vai ate o valor limitado
+  { 
+    servo.write(pos);                                            // Aplicação para o servo ir ate o local desejado 
+    delay(15);                                                   // delay para ir ate a posição
   }
-  for (pos = GrausFinal; pos >= GrausInicial; pos -= 1) {       // Volta para o valor inicial
-    servo.write(pos);                                           // Aplicação para o servo ir ate o local desejado 
-    delay(15);                                                  // delay para ir ate a posição
+  for (pos = GrausFinal; pos >= GrausInicial; pos -= 1)          // Volta para o valor inicial 
+  {  
+    servo.write(pos);                                            // Aplicação para o servo ir ate o local desejado 
+    delay(15);                                                   // delay para ir ate a posição determinada
   }
 }
