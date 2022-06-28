6e3c6381a9b19396b094f8e0166584681fe96857
ArthurFetterEducar <106825662+ArthurFetterEducar@users.noreply.github.com>
2022-06-28 13:47:17 -0300

    Update BoiaCima.ino

--- BoiaCima.ino
+++ BoiaCima.ino
@@ -1,20 +1,18 @@
-#define SENS_BOIA_C                        //Pino Analogico para a boia 
+#define SENS_BOIA_C                           //Pino Analogico para a boia 
 
 void setup() {
   
 pinMode(SENS_BOIA_B, INPUT);
 
 }
+  void loop() {
 
-void loop() {
+  int leitura_boiaC;                          //Variaveis de Leitura da Boia
 
-int leitura_boiaC;                         //Variaveis de Leitura da Boia
-
-leitura_boiaC = analogRead(SENS_BOIA_B);   //Leitura do pino
-                                           //Checa Esvaziamento
-  if(leitura_boiaC==0)                     //Nivel: Elevado               
+  leitura_boiaC = analogRead(SENS_BOIA_B);    //Leitura do pino
+                                              //Checa Esvaziamento 
+  if(leitura_boiaC==0)                        //Nivel: Elevado               
      return 'V';
-  if(leitura_boiaC==1)                     //Nivel: Normal          
-     return 'F';
-      
+  if(leitura_boiaC==1)                        //Nivel: Normal          
+     return 'F';   
 }
