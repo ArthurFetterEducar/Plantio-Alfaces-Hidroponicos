8d559809356e823ab0fa14b5185822ba137adb3c
Martininho <108107027+Martininho@users.noreply.github.com>
2022-06-28 14:11:57 -0300

    Update teclado_matricial.ino

--- teclado_matricial.ino
+++ teclado_matricial.ino
@@ -1,12 +1,9 @@
-
-
-
-
 int b1,b2,b3,b4,b5;
 int botao1,botao2,botao3,botao4,botao5;
 int comando = 0;
 
-void setup() {
+void setup() 
+{
  pinMode(botao1,INPUT);
  pinMode(botao2,INPUT);
  pinMode(botao3,INPUT);
@@ -14,10 +11,11 @@ void setup() {
  pinMode(botao5,INPUT); 
 }
 
-void loop() {
-
+void loop() 
+{
  // Botão da esquerda
- if(botao1 == 0 && b1 == 0){
+ if(botao1 == 0 && b1 == 0)
+ {
   comando = 1;             // Comando que sera retornado
   b1 = 1;                  // Impede a repetição 
  }
@@ -25,7 +23,8 @@ void loop() {
   b1 = 0;                  // Sinaliza que o boão foi solto
 
  // Botão da direita
- if(botao2 == 0 && b2 == 0){
+ if(botao2 == 0 && b2 == 0)
+ {
   comando = 2;             // Comando que sera retornado
   b2 = 2;                  // Impede a repetição 
  }
@@ -33,7 +32,8 @@ void loop() {
   b2 = 0;                  // Sinaliza que o boão foi solto
 
  // Botão de Cima 
-  if(botao3 == 0 && b3 == 0){
+  if(botao3 == 0 && b3 == 0)
+ {
   comando = 3;             // Comando que sera retornado
   b3 = 1;                  // Impede a repetição 
  }
@@ -41,7 +41,8 @@ void loop() {
   b3 = 0;                  // Sinaliza que o boão foi solto
   
  //Botão Baixo
-  if(botao4 == 0 && b4 == 0){
+  if(botao4 == 0 && b4 == 0)
+ {
   comando = 4;             // Comando que sera retornado
   b4 = 1;                  // Impede a repetição 
  }
@@ -49,7 +50,8 @@ void loop() {
   b4 = 0;                  // Sinaliza que o boão foi solto 
   
  //Botão de Confirmar
-  if(botao5 == 0 && b5 == 0){
+  if(botao5 == 0 && b5 == 0)
+ {
   comando = 5;             // Comando que sera retornado
   b5 = 1;                  // Impede a repetição 
  }
