84d9b23dcf900d4c24779f15f10367ace895e1f5
ArthurFetterEducar <106825662+ArthurFetterEducar@users.noreply.github.com>
2022-06-28 15:08:42 -0300

    Boia Baixo revisão tipografia

--- BoiaBaixo.ino
+++ BoiaBaixo.ino
@@ -1,18 +1,12 @@
 #define SENS_BOIA_B                        //Pino Analogico para a boia 
 
-void setup() {
+int leitura_boiaB;                         //Variaveis de Leitura da Boia 
 
-pinMode(SENS_BOIA_B,INPUT);
+pinMode(SENS_BOIA_B,INPUT); 
 
-}
 
-void loop() {
- int leitura_boiaB;                        //Variaveis de Leitura da Boia
-
- leitura_boiaB = analogRead(SENS_BOIA_B);  //Leitura do pino                                          
-                                           //Checa Esvaziamento
- if(leitura_boiaB==0)                      //Nivel da Agua: Baixo               
-      return 'V';
- if(leitura_boiaB==1)                      //Nivel: Normal          
-      return 'F';   
+int boiaBaixo() {
+ leitura_boiaB = digitalRead(SENS_BOIA_B);  //Leitura do pino 
+ 
+ return leitura_boiaB;
 }
