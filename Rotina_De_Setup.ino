c4eb297a81edc7352a6959c9db9ae4c8ea795b9e
Guirissi <106929790+Guirissi@users.noreply.github.com>
2022-06-29 15:14:53 -0300

    Update Rotina_De_Setup.ino

--- Rotina_De_Setup.ino
+++ Rotina_De_Setup.ino
@@ -9,58 +9,59 @@
 #define SENS_BOIA_A         //Boia De Cima
 #define LED_BOIA
 
-void setup() 
+void Rotina_Setup() 
 {
+   //Variaveis de Leitura Boias
+   int leitura_boiaA, leitura_boiaB;
+
    //Pinos Boia 
-  pinMode(SENS_BOIA_B, INPUT); 
-  pinMode(SENS_BOIA_A,INPUT);
-  pinMode(LED_BOIA,OUTPUT);
+   pinMode(SENS_BOIA_B, INPUT); 
+   pinMode(SENS_BOIA_A,INPUT);
+   pinMode(LED_BOIA, OUTPUT);
 
-  //Pinos LDR
-  pinMode(LDR1, INPUT);
-  pinMode(LDR2, INPUT);
-  pinMode(LED_LDR, OUTPUT);
+   //Pinos LDR
+   pinMode(LDR1, INPUT);
+   pinMode(LDR2, INPUT);
+   pinMode(LED_LDR, OUTPUT);
 
- //Exec Funçoes 
-  
- //Variaveis de Leitura LDR
- int  leitura_ldr1, leitura_ldr2;
- 
- //Checagem Obstruçao LDR 
- leitura_ldr1 = analogRead(LDR1);
- leitura_ldr2 = analogRead(LDR2);
- 
- //------------------LEITURA DE OBSTRÇÃO------------------------------
- if((leitura_ldr1 > leitura_ldr2 + OBSTRUCAO) || (leitura_ldr2 > leitura_ldr1 + OBSTRUCAO))
- {
-  digitalWrite(HIGH,LED_LDR);
-  NotificaUsuario(); 
-  NotificaLCD();
- }
- 
- //----------------Leitura Sensores Boia -----------------------------
- 
- //Variaveis de Leitura Boias
- int leitura_boiaA, leitura_boiaB;
- 
- //Leitura dos pinos 
- leitura_boiaA = analogRead(SENS_BOIA_C);
- leitura_boiaB = analogRead(SENS_BOIA_B);
- 
- //Checa esvaziamento
- if(leitura_boiaB == 0)
- {
+   //Exec Funçoes 
+
+   //Variaveis de Leitura LDR
+   int  leitura_ldr1, leitura_ldr2;
+
+   //Checagem Obstruçao LDR 
+   leitura_ldr1 = analogRead(LDR1);
+   leitura_ldr2 = analogRead(LDR2);
+
+   //------------------LEITURA DE OBSTRÇÃO------------------------------
+   if( (leitura_ldr1 > (leitura_ldr2 + OBSTRUCAO) ) ||
+     (leitura_ldr2 > (leitura_ldr1 + OBSTRUCAO) )
+   {
+   digitalWrite(HIGH, LED_LDR);
+   NotificaUsuario(); 
+   NotificaLCD();
+   }
+
+   //----------------Leitura Sensores Boia -----------------------------
+
+   //Leitura dos pinos 
+   leitura_boiaA = analogRead(SENS_BOIA_C);
+   leitura_boiaB = analogRead(SENS_BOIA_B);
+
+   //Checa esvaziamento
+   if(leitura_boiaB == 0)
+   {
     digitalWrite(HIGH, LED_BOIA);
     NotificaUsuario(); 
     NotificaLCD();
-  }
-  
-  //Checa excesso
-  if(leitura_boiaA==1)
-  {
+   }
+
+   //Checa excesso
+   if(leitura_boiaA==1)
+   {
     digitalWrite(HIGH,LED_BOIA);
     NotificaUsuario(); 
     NotificaLCD(); 
-  }
+   }
 }
 
