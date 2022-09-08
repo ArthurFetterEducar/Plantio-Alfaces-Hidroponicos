be35b6d586338e519e1da0b89f780b856c7e61b1
Guirissi <106929790+Guirissi@users.noreply.github.com>
2022-09-07 21:58:03 -0300

    ContagemDeTempo

---
+++ tempo_esp.ino
@@ -0,0 +1,43 @@
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+const char *ssid     = "TP-Link_diego";
+const char *password = "99229129";
+
+const long utcOffsetInSeconds = 20;
+
+char daysOfTheWeek[7][12] = {"Dommingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado"};
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "br.pool.ntp.org", -10656);
+
+void setup(){
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+ 
+  timeClient.begin();
+}
+
+void loop() {
+  timeClient.update();
+   timeClient.forceUpdate();
+
+  Serial.print(daysOfTheWeek[timeClient.getDay()]);
+  Serial.print(", ");
+  Serial.print(timeClient.getHours());
+  Serial.print(":");
+  Serial.print(timeClient.getMinutes());
+  Serial.print(":");
+  Serial.println(timeClient.getSeconds());
+  
+
+  delay(1000);
+}
