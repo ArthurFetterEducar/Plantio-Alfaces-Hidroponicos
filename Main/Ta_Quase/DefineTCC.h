f0bfaded22c1f844279959fc17db550759cbeb32
Guirissi <106929790+Guirissi@users.noreply.github.com>
2022-10-13 10:30:09 -0300

    Add files via upload

---
+++ Main/Ta_Quase/DefineTCC.h
@@ -0,0 +1,49 @@
+#include "Arduino.h"
+
+#define PINO_PELTIER_F 26
+#define PINO_PELTIER_Q 25
+
+#define PWM_BAIXO 50
+#define PWM_MEDIO 150
+#define PWM_ALTO 200
+#define PWM_DESLIGADO 0
+
+#define COOLER_F 15 
+#define COOLER_Q 16
+
+#define ADC_VREF_mV    3300.0 // in millivolt
+#define ADC_RESOLUTION 4096.0
+#define PINO_LM35_Q 36
+#define PINO_LM35_F 35
+
+#define PINO_DHT22 4
+#define DHTTYPE 22
+
+#define PINO_PIEZO 15
+
+#define PINO_BOMBA 2
+
+#define PINO_SERVO 19
+
+//PINO OLED
+#define PINO_SCL 21
+#define PINO_SDA 22
+
+//PINO DO TECLADO
+#define PINO_TECLA_CIMA 32
+#define PINO_TECLA_DIRT 26
+#define PINO_TECLA_ESQ 25
+#define PINO_TECLA_BAIXO 33
+#define PINO_TECLA_CONFIRMA 14
+#define PINO_TECLA_COMUM 27
+
+//Sensor Boia
+#define PINO_BOIA1 12
+#define PINO_BOIA2 13
+
+//Define de Tempos
+#define MinutoBombaL 1
+#define MinutoBombaD 1
+
+#define MinutoDHTR 1
+//#define
---
+++ Main/Ta_Quase/TCC.h
@@ -0,0 +1,18 @@
+#include "DefineTCC.h"
+void SetupTCC();
+void LowPin();
+
+int Minuto();
+int Hora();
+
+float Temperatura();
+float Umidade();
+
+float LeituraLmQ();
+float LeituraLmF();
+
+void LigaPeliterQ(float temp);
+void DesligaPeliterQ();
+
+void LigaPeliterF(float temp);
+void DesligaPeliterF();
